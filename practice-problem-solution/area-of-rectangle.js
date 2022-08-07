ed43a2c7dcc7537b6c2e82bcc5d6415e8d5f41f1
fahimahammed <fahimahammed.cse@gmail.com>
2022-08-08 00:09:36 +0600

    area of rectangle

---
+++ practice-problem-solution/area-of-rectangle.js
@@ -0,0 +1,9 @@
+// Write a function named findArea() that will take height and width of a rectangle and will return the area of rectangle.
+
+function findArea(height, width) {
+    const area = height * width;
+    return area;
+}
+
+const areaOfRectangle = findArea(4, 5);
+console.log(areaOfRectangle);
